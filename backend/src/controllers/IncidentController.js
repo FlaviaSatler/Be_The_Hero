0da59674a3563414636f84c75c07e650a5c96e40
Flavia Satler <flaviasatler@hotmail.com>
2020-03-25 16:33:03 -0300

    feat(Day02): add counter incidents

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -6,10 +6,15 @@ module.exports = {
 
     const {page = 1} = request.query;
 
+    const [count] = await connection('incidents').count();
+
+    console.log(count)
     const incidents = await connection('incidents')
     .limit(5)
     .offset((page - 1) * 5)
     .select('*');
+
+   response.header('X-Total-Count', count['count(*)']) 
 		
 		return response.json(incidents);
 	},
