6ffae232fd593cb3cc6fa2b703f5469296fed993
Flavia Satler <flaviasatler@hotmail.com>
2020-03-26 10:58:53 -0300

    feat(Day03): state and immutability concepts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import Header from './Header'
 
 
 function App() {
+
+  const [counter, setCounter] = useState(0);
+
+  //retorna um array com duas posições: [valor, funçãoDeAtualização]
+
+  function increment() {
+    setCounter(counter + 1)
+    console.log(counter)
+  }
+
   return (
-    <h1>Hello Hero!</h1>
+    <div>
+      <Header>Contador: {counter} </Header>
+
+      <button onClick={increment}>Incrementar</button>
+    </div>
   );
 }
 
